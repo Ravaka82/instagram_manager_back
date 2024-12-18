0eef11ad8f72d71615314cc13fdff5cc1928aa52
Ravaka82 <89504880+Ravaka82@users.noreply.github.com>
2024-12-18 12:51:15 +0300

    Cross-Origin Resource Sharing (CORS)

--- instagram_manager_back/settings.py
+++ instagram_manager_back/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'instagram',
     'users',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -51,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'instagram_manager_back.urls'
@@ -145,6 +147,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 APPEND_SLASH = False
 
 CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",  # React app
+]
 
 DEBUG = True
 
